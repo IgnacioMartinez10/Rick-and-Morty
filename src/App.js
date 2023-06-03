daaa29013b59366961713b945f5900e2b694ba69
Ignacio Martinez <ignaciomartinez923@gmail.com>
2023-06-03 10:49:44 -0300

    Modificando componentes

--- src/App.js
+++ src/App.js
@@ -1,16 +1,33 @@
-import './App.css';
-import Card from './components/Card/Card';
-import Cards from './components/Cards/Cards';
-import SearchBar from './components/SearchBar/SearchBar.jsx';
-import characters, { Rick } from './data.js';
+import "./App.css";
+import NavBar from "./components/NavBar/NavBar";
+import { useState } from "react";
+import Cards from "../src/components/Cards/Cards";
 
 function App() {
-   return (
-      <div className='App'>
-         <SearchBar onSearch={(characterID) => window.alert(characterID)} />
-         <Cards characters={characters} />
-      </div>
-   );
+  const [characters, setCharacters] = useState([]);
+
+  const onSearch = () => {
+    const newCharacter = {
+      id: 1,
+      name: "Rick Sanchez",
+      status: "Alive",
+      species: "Human",
+      gender: "Male",
+      origin: {
+        name: "Earth (C-137)",
+        url: "https://rickandmortyapi.com/api/location/1",
+      },
+      image: "https://rickandmortyapi.com/api/character/avatar/1.jpeg",
+    };
+    
+    setCharacters([...characters, newCharacter])
+  };
+  return (
+    <div className="App">
+      <NavBar onSearch={onSearch} />
+      <Cards characters={characters}/>
+    </div>
+  );
 }
 
 export default App;
--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,8 +1,22 @@
+import { useState } from 'react';
+import styles from './SearchBar.module.css'
+
 export default function SearchBar({onSearch}) {
+
+   const saveOnSearch = () => {
+      onSearch(id)
+   }
+
+   const [id, setId] = useState("");
+
+   const handleChange = (event) =>{
+      const saveId = event.target.value
+      setId(saveId)
+   }
    return (
-      <div>
-         <input type='search' />
-         <button onClick={onSearch}>Agregar</button>
+      <div className={styles.container}>
+         <input onChange={handleChange} type='search'placeholder='Escriba el ID del personaje que desee agregar' />
+         <button onClick={saveOnSearch}>Agregar</button>
       </div>
    );
 }
